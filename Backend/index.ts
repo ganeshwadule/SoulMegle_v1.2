e24c0f069f4c923aad0bd3d4d0195c49119dbc29
ganeshwadule <uniquegbw09@gmail.com>
2025-02-23 15:24:47 +0530

    socket setup

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,1 +1,19 @@
-console.log("hello omegle")
+// const http = require("http")
+// const express = require("express")
+// const {Server,Socket} = require("socket.io")
+import http from 'http';
+import express from 'express';
+import {Server,Socket} from "socket.io";
+
+const app = express();
+
+const server = http.createServer(http);
+const io = new Server(server);
+
+io.on('connection',(socket:Socket)=>{
+    console.log("a user connected");
+})
+
+server.listen(3000,()=>{
+    console.log("Server Started");
+})
